67f54cc555255f39304d96cdcdbd84bb07785f34
Samllo95 <ulloajose34@gmail.com>
2024-11-19 00:56:46 -0600

    init

---
+++ src/test/java/com/samllo95/curso_springboot/CursoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samllo95.curso_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
